7fe5451dbfb14289dac2edc1643a678464ec5081
MeivanK20 <mouzonglandry@gmail.com>
2025-10-19 00:20:31 +0100

    Mise à jour du code et correctifs

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBwQ0TTT4QGCu5V_vnnExG3jZSdCjldn_4",
-  authDomain: "my-immo-a8674.firebaseapp.com",
-  projectId: "my-immo-a8674",
-  storageBucket: "my-immo-a8674.firebasestorage.app",
-  messagingSenderId: "451589935533",
-  appId: "1:451589935533:web:f9b062a92d84b0997f9124",
+  apiKey: "AIzaSyD27ISSfrPPahidXPWRpjOVjGqPdUgMQgY",
+  authDomain: "my-immo-8d85d.firebaseapp.com",
+  projectId: "my-immo-8d85d",
+  storageBucket: "my-immo-8d85d.firebasestorage.app",
+  messagingSenderId: "651818479687",
+  appId: "1:651818479687:web:4d701431c5253ccf304162",
 };
 
 const app = initializeApp(firebaseConfig);
